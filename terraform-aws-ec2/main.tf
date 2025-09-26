3dce40df8fd7b365b6de9d4ccacfdbd5f0d5c735
khushboo-sah <khushboo.sah067@gmail.com>
2025-09-26 18:06:06 +0300

    creating logs

--- terraform-aws-ec2/main.tf
+++ terraform-aws-ec2/main.tf
@@ -1,24 +1,13 @@
 provider "aws" {
-   region                   = "eu-central-1"
-   shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+   region                   = "eu-north-1"
 }
 
+
 resource "aws_instance" "ec2_example" {
-   ami           = "ami-0767046d1677be5a0"
-   instance_type =  "%{ if var.instance_type != "" }${var.instance_type}%{ else }t2.micro%{ endif }"
+   ami           = "ami-0a716d3f3b16d290c"
+   instance_type =  "t3.micro"
+
    tags = {
            Name = "Terraform EC2"
    }
-}
-
-
-variable "instance_type" {
-   description = "Instance type t2.micro"
-   type        = string
-   default     = ""
-
-#  validation {
-#   condition     = can(regex("^[Tt][2-3].(nano|micro|small)", var.instance_type))
-#   error_message = "Invalid Instance Type name. You can only choose - t2.nano,t2.micro,t2.small"
-# }
 }
