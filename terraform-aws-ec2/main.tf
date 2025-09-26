b4297abc89fab9d7d8457fefea5b4609b5c8d01e
khushboo-sah <khushboo.sah067@gmail.com>
2025-09-26 18:49:42 +0300

    Terraform validation

--- terraform-aws-ec2/main.tf
+++ terraform-aws-ec2/main.tf
@@ -1,13 +1,23 @@
 provider "aws" {
    region                   = "eu-north-1"
 }
+variable "instance_type" {
+  description = "Type of instance to use"
+  type        = string
+  default     = "t3.large"
 
+  validation {
+    condition     = can(regex("^[Tt][2-3]\\.(nano|micro|small)$", var.instance_type))
+    error_message = "Invalid Instance Type name. You can only choose: t2.nano, t2.micro, t2.small, t3.nano, t3.micro, t3.small"
+  }
+}
 
 resource "aws_instance" "ec2_example" {
-   ami           = "ami-0a716d3f3b16d290c"
-   instance_type =  "t3.micro"
+  ami           = "ami-0a716d3f3b16d290c"
+  instance_type = var.instance_type
 
-   tags = {
-           Name = "Terraform EC2"
-   }
+  tags = {
+    Name = "Terraform EC2"
+  }
 }
+
