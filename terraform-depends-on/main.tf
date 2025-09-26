5d224134b6dd7389a404228a11f2e81a02e3f0bd
khushboo-sah <khushboo.sah067@gmail.com>
2025-09-26 14:51:44 +0300

    Terraform-depends-on

--- terraform-depends-on/main.tf
+++ terraform-depends-on/main.tf
@@ -1,18 +1,24 @@
+
+
 provider "aws" {
-  region                   = "eu-central-1"
-  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+   region     = "eu-north-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = file("~/.ssh/aws_ec2_terraform.pub")
 }
 
 resource "aws_s3_bucket" "my_test_bucket" {
-  bucket = "test-bucket-2-for-import"
+  bucket = "test-bucket-1-for-import-45"
   tags = {
     "name" = "test-bucket"
   }
 }
 
 resource "aws_instance" "ec2_example" {
-   ami           = "ami-0767046d1677be5a0"
-   instance_type =  "t2.micro"
+   ami           = "ami-0a716d3f3b16d290c"
+   instance_type =  "t3.micro"
 
    tags = {
            Name = "Terraform EC2"
